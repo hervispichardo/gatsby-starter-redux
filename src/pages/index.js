c957bc537f4baf9dfb5705aa49a77b31ae618690
Hervis Pichardo <hervis15@gmail.com>
2018-07-24 18:26:57 -0400

    test redux implement

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Link from 'gatsby-link';
+import { loadOffices as loadOfficesAction } from '../state/offices';
 
-const IndexPage = () => (
+const Index = ({ offices, current}) => (
   <div>
     <h1>Hi Hervis</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>Oficinas: {offices}</p>
+    <p>current: {current}</p>
+    <Link to="/About/">Go to page 2</Link>
   </div>
 );
 
-export default IndexPage;
+export default connect(
+  state => ({
+    offices: state.offices.list,
+    current: state.offices.current,
+  }),
+  dispatch => ({ loadOffices: () => loadOfficesAction() }),
+)(Index);
+
--- src/state/index.js
+++ src/state/index.js
@@ -1,4 +1,8 @@
 import { combineReducers } from 'redux';
 import app from './app';
+import offices from './offices';
 
-export default combineReducers({ app });
+export default combineReducers({
+  app,
+  offices
+});
